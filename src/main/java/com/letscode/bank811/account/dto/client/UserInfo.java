bb919c3e7e970f81b98b273ed694ab87229444cf
Marisa Sales <73047662+marisasales@users.noreply.github.com>
2022-04-25 14:49:12 -0300

    feat: add UserInfo

---
+++ src/main/java/com/letscode/bank811/account/dto/client/UserInfo.java
@@ -0,0 +1,11 @@
+package com.letscode.bank811.account.dto.client;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class UserInfo {
+
+  private String name;
+  private String cpf;
+}
